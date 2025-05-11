6694a878972dc2b926ebf08e7e113e148e78e424
gupx9 <muhtasimfaiyaz07@gmail.com>
2025-05-11 12:13:21 +0600

    changed theme to a lighter version

--- recipient/savings_account.php
+++ recipient/savings_account.php
@@ -106,7 +106,7 @@ try {
         
     </style>
 </head>
-<body style="background: linear-gradient(to right, rgb(92, 74, 91), rgb(204, 205, 206));">
+<body style="background: linear-gradient(to right, rgba(78, 82, 85, 0.51), rgba(216, 196, 215));">
 
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgba(60, 50, 65, 0.6);">
     <div class="container"> 
